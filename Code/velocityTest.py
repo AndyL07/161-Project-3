89c0bd8939b85341f1f2414968f9a31b57862be1
Eli-Lin-7 <elinmail77@gmail.com>
2025-12-07 14:36:31 -0500

    Add files via upload

--- Code/velocityTest.py
+++ Code/velocityTest.py
@@ -1,37 +1,93 @@
+# File Name: <smartStraight.py>
+# Date: <11/13/25>
+# By: <Andrew Lullo>
+# <lullo>
+# Section: <4>
+# Team: <59>
+
+
+# Imports
+# --------------------------
+from basehat import LineFinder
+# from payloadFunctions import Payload
+from basehat import IMUSensor
+from driveFunctions import Drive
+from Telemetry import Telemetry
+from Timer import Timer
 import time
-from buildhat import Motor
-SPEED = 1
 
+# Constants
+# ----------------------
+WAIT_TIME = 0.05
+STRAIGHT_SPEED = 0.5
+SWEEP_SPEED = 0.3
+CORRECTION_COEFF = 1
+MAX_ANGLE = 30
+
+# Running Code
+# ------------------------
 def main():
+    # set the pin to be used
+    # if sensor is plugged into port D5, pin1 should be 5
+    # make sure to only plug in LineFinder Sensor to digital ports of the Grove BaseHAT (D5, D16, D18, D22, D24, D26)
+    linePin = 5
     
-    leftMotor = Motor('A')
-    rightMotor = Motor('B')
-
-    # Function to print the current speed, position and absolute position of the motors
-    def handle_motor(speed, pos, apos):
-        """Motor data
-
-        :param speed: Speed of motor
-        :param pos: Position of motor
-        :param apos: Absolute position of motor
-        """
-        print("Motor", speed, pos, apos)
-
-    try: 
+    VELOCITYTEST = int(input("Enter speed: "))
+    VELOCITYTEST = (VELOCITYTEST / 54.757) + 0.003853
+    # Initializing the sensor so the function within the class can be used
+    lineFinder = LineFinder(linePin)
+    
+    # Initializing the IMU so the example can utilize the sensor
+    IMU = IMUSensor()
+    
+    newTime = 0
+    newGyroZ = 0
+    angle = 0
+    
+    runTime = Timer()
+    tel = Telemetry(runTime)
+    d = Drive(tel)   
+    
+    try:
+        start_time = time.time()
         while True:
-            try:
-                leftMotor.pwm(SPEED)
-                rightMotor.pwm(SPEED)
+            if(time.time() - start_time >= 5):
+                break
+            try: 
+                # update and read the values of the lineFinder
+                lineFound = lineFinder.value
+
+                # Sets values of previous loop
+                oldTime = newTime
+                oldGyroZ = newGyroZ
+                
+                newTime = runTime.currTime()
+                x, y, newGyroZ = IMU.getGyro()
+                
+                angle += (newTime - oldTime) * (oldGyroZ + newGyroZ) / 2
+                tel.add(angle, "Angle")
+
+                #d.fullSmartStraight(STRAIGHT_SPEED, angle)
+                d.fullSmartStraight(VELOCITYTEST, angle)
+#                 # Test what regular would be like
+               # d.goStraight(STRAIGHT_SPEED)
+    
+                    # Tellemetry
+                print(VELOCITYTEST)
+                print(tel)
+                tel.reset()
+
+                # Wait time
+                time.sleep(WAIT_TIME)
+                
 
             except IOError:
                 print ("\nError occurred while attempting to read values.")
                 break
 
     except KeyboardInterrupt:
+        d.stop()
         print("\nCtrl+C detected. Exiting...")
-        leftMotor.stop()
-        rightMotor.stop()
-
 
 if __name__ == '__main__':
     main()
